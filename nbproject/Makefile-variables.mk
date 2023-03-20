48dd62bd0d2acd3362d0f1664e4bc7724d7a1d78
DenilDenilson <jordanpreseleccion@gmail.com>
2023-03-20 12:26:06 -0500

    Init a lot of projects using MMJ TRAINER MINI

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TrainerMini.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TrainerMini.X.production.hex
